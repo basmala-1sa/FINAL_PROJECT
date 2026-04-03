ac15c76e82ec33763e94b9af916194d01085c3a6
basmala-1sa <basmala.saouli@univ-constantine2.dz>
2026-04-04 00:58:51 +0200

    connected Django to MySQL

--- backend/settings.py
+++ backend/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stageio_db',
+        'USER': 'root',
+        'PASSWORD': '',        
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
